d380ef5190723677b374f5b43761daad2f48e076
chiragsurana <57294330+chiragsurana@users.noreply.github.com>
2021-08-26 17:23:16 +0530

    Add files via upload

--- src/Components/Class.js
+++ src/Components/Class.js
@@ -3,8 +3,16 @@ import ClassComponent from './ClassComponent';
 import './Class.css';
 import { useDispatch, useSelector } from 'react-redux';
 import {saveIt, selectItems} from '../StoreReducers/classSlice';
-import { ArrowRight } from '@material-ui/icons';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    BrowserRouter
+  } from "react-router-dom";
+/*import { ArrowRight } from '@material-ui/icons';*/
 import SideNav from './SideNav';
+import ListComponent from './ListComponent';
 const Class = () => {
 
     
@@ -12,7 +20,11 @@ const [checkValue,setCheckValue]=useState(true);
 const [clicked,setClicked] =useState(false);
 const [intitle,setIntitle]=useState("");
 const [inteacher,setInteacher] =useState(" "); 
-
+ var start ,end ;
+const [s1,setS1]=useState("");
+const [s2,setS2]=useState("");
+const [e1,sete1]=useState("");
+const [e2,sete2]=useState("");
 const dispatch =useDispatch();
   const items=useSelector(selectItems);
 
@@ -21,11 +33,14 @@ const dispatch =useDispatch();
       setInteacher("");
       setIntitle("");
       setTimeout(  setClicked(false), 5000);
-      
+      start =parseFloat(s1.s2);
+      end=parseFloat(e1.e2);
       dispatch(saveIt( {
           title:intitle,
           instructor:inteacher,
           id:Date.now(),
+          start_time:start,
+          end_time:end
       }))
 
   } 
@@ -33,11 +48,13 @@ const dispatch =useDispatch();
      
   
     return (
+        <>
+          <BrowserRouter> 
         <div className="class">
             <div className="class_available">
-           
+          
             
-            {   items.length>0 ?   <h1> You are enrolled in the classes</h1>:   <h1>You are not enrolled yet !!!!</h1>   }
+            {   items.length>0 ?   <h1> You are enrolled in {items.length} classes</h1>:   <h1>You are not enrolled yet !!!!</h1>   }
           
             <button className="class_button" onClick={()=>setClicked(!clicked)} > <span > </span> Click Here to Add a Class  </button>
             
@@ -47,24 +64,42 @@ const dispatch =useDispatch();
          
              <label for="lname">instructor :</label>
              <input type="text" value={inteacher} onChange={(e)=>setInteacher(e.target.value)}    placeholder="Enter the instructor Name" required/>
-         
+             <label>Start Time:</label> <input  value={s1} onChange={(e)=>setS1(e.target.value)}type="number" min="00" max="23"></input><input value={s2} onChange={(e)=>setS2(e.target.value)} type="number"min="00" max="59"step="1" ></input>
+             <label>End Time:</label>  <input value={e1} onChange={(e)=>sete1(e.target.value)}type="number" min="00" max="23"></input><input  value={e2} onChange={(e)=>sete2(e.target.value)} type="number" min="00"max="59"step="1"></input>
              <input type="checkbox" onClick={()=>setCheckValue(!checkValue)} ></input>You affirm to make a new class<br></br>
           
              <button onClick={clickedddd} className={checkValue ?"white":"green"} disabled={checkValue}>Click</button>
-             
-
-         </form>
-           
+             </form>
+       
+         <Switch>
+         <Router path="/grid"> <div className="enroled_classes">
             {   items.length>0 &&
                 <div className="class_enrolled">
-               {items.map(ite =>(  <ClassComponent title={ite.title} instructor={ite.instructor}  id={ite.id}/>))}
+               {items.map(ite =>(  <ClassComponent title={ite.title} instructor={ite.instructor}  id={ite.id} start_time={ite.start_time} end_time={ite.end_time}/>))}
                 </div>
             }
-           
+            </div>
+            </Router> 
+            <Router path="/list">
+                <div className="enrolled_classes_list">
+                    {
+                        items.length>0 &&
+                        <div className="list_class_enrolled">
+                           
+         {items.map(ite =>(  <ListComponent title={ite.title} instructor={ite.instructor}  id={ite.id} endTime={ite.end_time} startTime={ite.start_time}  />))}
+                 
+                            </div>
+                    }
+                </div>
+            </Router>
+            </Switch>
            
             </div>
+           <SideNav/>
         </div>
+        </BrowserRouter>
+        </>
     )
-}
+};
 
-export default Class
+export default Class;
--- src/Components/ClassComponent.js
+++ src/Components/ClassComponent.js
@@ -4,10 +4,11 @@ import React from 'react'
 import { useDispatch } from 'react-redux';
 import {deleteIt }from '../StoreReducers/classSlice';
 import './ClassComponent.css';
-const ClassComponent = ({title,instructor,  id}) => {
+const ClassComponent = ({title,instructor,  id,start_time,end_time}) => {
    
     const dispatch =useDispatch();
     const deleted=( e)=>{
+        alert("Are you sure you want  to delete it");
         e.preventDefault();
         dispatch(deleteIt({
             
@@ -16,23 +17,27 @@ const ClassComponent = ({title,instructor,  id}) => {
     }
     return (
         <div className="classComponent">
-        <img className="classComponent_img" src="https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8c3R1ZHl8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60" alt =""/>
+        <img className="classComponent_img" src="https://images.unsplash.com/photo-1505682499293-233fb141754c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=751&q=80" alt =""/>
    
          <div className ="classComponent_middle"> 
          <Avatar className="avatar"/>
          <div className="classComponent_middleText">
-        <h3    >{title.length<15 ?title : title.substring(0,15)+ "..."}</h3>
+        <h3    >{title.length<15 ?title : title.substring(0,12)+ "..."}</h3>
        <p>  by:{instructor}</p> 
-         
+         <p>{start_time} - {end_time}</p>
         </div>
         </div>
-        <div className="classComponent_footer" >
-         <Class/>
+        <div className="clt_footer" >
+         <Class />
         
          <Folder/>
-       
-         <button  onClick={deleted} className ="classComponent_delete"><Delete /></button>
+        
+
+         <Delete    onClick={deleted} /> 
          
+   
+ 
+ 
         </div>
         </div>
     )
